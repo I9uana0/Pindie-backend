02ce21f16eaabb6ec1fd1e791640c5db8aac54b2
I9uana0 <ivchi06@gmail.com>
2024-05-25 16:11:39 +0300

    готовый проект админки пинди

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const usersRouter = require('./routes/users');
+const gamesRouter = require('./routes/games');
+const categoriesRouter = require('./routes/categories');
 
-const PORT = 3000;
+const connectToDatabase = require('./database/connect');
+const cors = require('./cors');
 
 const app = express();
+const PORT = 3000;
+
+connectToDatabase();
+
+app.use(
+    cors,
+    bodyParser.json(),
+    express.static(path.join(__dirname, 'public')),
+    usersRouter,
+    gamesRouter,
+    categoriesRouter
+);
 
 app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}`)
-})
+});
