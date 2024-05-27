5110f225cfcd2ed067f6d85d1a2a85f9e244c524
I9uana0 <ivchi06@gmail.com>
2024-05-27 19:46:47 +0300

    готовый бэкенд админки с добавлением, редактированием и удалением данных

--- middlewares/users.js
+++ middlewares/users.js
@@ -1,13 +1,97 @@
-// Файл middlewares/users.js
-
-// Импортируем модель
 const users = require('../models/user');
 
 const findAllUsers = async (req, res, next) => {
-    // По GET-запросу на эндпоинт /users найдём все документы пользователей
     req.usersArray = await users.find({});
     next();
 }
 
-// Экспортируем функцию поиска всех пользователей
-module.exports = findAllUsers;
+const createUser = async (req, res, next) => {
+    console.log('POST /users');
+    try {
+        req.users = await users.create(req.body);
+        next();
+    } catch {
+        res.setHeader('Content-Type', 'application/json');
+        res.status(400).send(JSON.stringify({ message: "Ошибка добавления пользователя" }))
+    }
+}
+
+const findUserById = async (req, res, next) => {
+    console.log("GET /users/:id");
+    try {
+        req.user = await users.findById(req.params.id);
+        next();
+    } catch (err) {
+        res.setHeader("Content-Type", "application/json");
+        res.status(404).send(JSON.stringify({ message: "Пользователь не найден" }));
+    }
+}
+
+const updateUser = async (req, res, next) => {
+    try {
+        req.user = await users.findByIdAndUpdate(req.params.id, req.body);
+        next()
+    } catch (err) {
+        res.setHeader("Content-Type", "application/json");
+        res.status(400).send(JSON.stringify({ message: "Ошибка обновления пользователя" }));
+    }
+}
+
+const deleteUser = async (req, res, next) => {
+    try {
+        req.user = await users.findByIdAndDelete(req.params.id);
+        next();
+    } catch (err) {
+        res.setHeader("Content-Type", "application/json");
+        res.status(400).send(JSON.stringify({ message: "Ошибка удаления пользователя" }));
+    }
+}
+
+const checkEmptyNameAndEmailAndPassword = async (req, res, next) => {
+    if (
+        !req.body.username ||
+        !req.body.email ||
+        !req.body.password
+    ) {
+        res.setHeader("Content-Type", "application/json");
+        res.status(400).send(JSON.stringify({ message: "Заполни все поля" }));
+    } else {
+        next();
+    }
+}
+
+const checkEmptyNameAndEmail = async (req, res, next) => {
+    if (
+        !req.body.username ||
+        !req.body.email
+    ) {
+        res.setHeader("Content-Type", "application/json");
+        res.status(400).send(JSON.stringify({ message: "Заполни все поля" }));
+    } else {
+        next();
+    }
+
+}
+
+const checkIsUserExists = async (req, res, next) => {
+    const isInArray = req.usersArray.find((user) => {
+        return req.body.email === user.email;
+    });
+    if (isInArray) {
+        res.setHeader("Content-Type", "application/json");
+        res.status(400).send(JSON.stringify({ message: "Пользователь с таким email уже существует" }));
+    } else {
+        next();
+    }
+};
+
+module.exports = {
+    findAllUsers,
+    createUser,
+    findUserById,
+    updateUser,
+    deleteUser,
+    checkEmptyNameAndEmailAndPassword,
+    checkEmptyNameAndEmail,
+    checkIsUserExists,
+};
--- routes/categories.js
+++ routes/categories.js
@@ -1,12 +1,40 @@
-// Создаём роут для запросов категорий 
 const categoriesRouter = require('express').Router();
 
-// Импортируем вспомогательные функции
-const findAllCategories = require('../middlewares/categories');
-const sendAllCategories = require('../controllers/categories');
+const { findAllCategories, createCategory, findCategoryById, updateCategory, deleteCategory, checkIsCategoryExists, checkEmptyName } = require('../middlewares');
+const { sendAllCategories, sendCategoryCreated, sendCategoryById, sendCategoryUpdated, sendCategoryDeleted } = require('../controllers');
 
-// Обрабатываем GET-запрос с роутом '/categories'
-categoriesRouter.get('/categories', findAllCategories, sendAllCategories);
+categoriesRouter.get(
+    '/categories',
+    findAllCategories,
+    sendAllCategories
+);
 
-// Экспортируем роут для использования в приложении — app.js
-module.exports = categoriesRouter;
+categoriesRouter.get(
+    '/categories/:id',
+    findCategoryById,
+    sendCategoryById
+)
+
+categoriesRouter.post(
+    "/categories",
+    findAllCategories,
+    checkIsCategoryExists,
+    checkEmptyName,
+    createCategory,
+    sendCategoryCreated
+);
+
+categoriesRouter.put(
+    "/categories/:id",
+    checkEmptyName,
+    updateCategory,
+    sendCategoryUpdated
+);
+
+categoriesRouter.delete(
+    '/categories/:id',
+    deleteCategory,
+    sendCategoryDeleted
+)
+
+module.exports = { categoriesRouter };
