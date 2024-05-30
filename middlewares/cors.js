acdb6465a91e634b3cb82ff72f181ab45d7d3521
I9uana0 <ivchi06@gmail.com>
2024-05-30 13:40:34 +0300

    fixed cors

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -10,9 +10,10 @@ const allowedCores = [
 const cors = (req, res, next) => {
     const { origin } = req.headers;
     if (allowedCores.includes(origin)); {
-        res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
-        res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization");
+        res.header('Access-Control-Allow-Origin', origin);
     }
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
+    res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization");
     next();
 }
 
